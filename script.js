74e8cd209229006b9bfcff1c9c42f75dbfb61fa6
navid1256 <navid.syndicate@gmail.com>
2026-06-19 15:21:02 +0330

    change the language from english to persian for hardware section add power to the hardware section add DVD writer and CD Writer radio button

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ const ramRowTemplate = ramSlots.querySelector('[data-ram-row]').cloneNode(true);
 const hardNumberSelect = document.getElementById('hard-number');
 const storageNumber = document.getElementById('storage-number');
 const hardRowTemplate = storageNumber.querySelector('[data-hard-row]').cloneNode(true);
-const dvdWriterNumberSelect = document.getElementById('dvd-writer-number');
-const dvdWriterNumber = document.getElementById('dvd-writer-numbers');
-const dvdWriterRowTemplate = dvdWriterNumber.querySelector('[data-dvd-writer-row]').cloneNode(true);
 const dvdWriterEnabled = document.getElementById('dvd-writer-enabled');
 const dvdWriterSection = document.querySelector('.dvd-writer-section');
 
@@ -26,6 +23,7 @@ function updateRamFieldIds(row, rowNumber) {
         label.htmlFor = `${baseFor}-${rowNumber}`;
     });
 
+
     row.querySelectorAll('select, input').forEach((field) => {
         const baseId = field.id.replace(/-\d+$/, '');
         field.id = `${baseId}-${rowNumber}`;
@@ -33,18 +31,6 @@ function updateRamFieldIds(row, rowNumber) {
 }
 
 function updateHardFieldIds(row, rowNumber) {
-    row.querySelectorAll('label[for]').forEach((label) => {
-        const baseFor = label.htmlFor.replace(/-\d+$/, '');
-        label.htmlFor = `${baseFor}-${rowNumber}`;
-    });
-
-    row.querySelectorAll('select, input').forEach((field) => {
-        const baseId = field.id.replace(/-\d+$/, '');
-        field.id = `${baseId}-${rowNumber}`;
-    });
-}
-
-function updateDVDWriterFieldIds(row, rowNumber) {
     row.querySelectorAll('label[for]').forEach((label) => {
         const baseFor = label.htmlFor.replace(/-\d+$/, '');
         label.htmlFor = `${baseFor}-${rowNumber}`;
@@ -95,30 +81,9 @@ function renderHardRows() {
     }
 }
 
-function renderDVDWriterRows() {
-    const rowCount = Number(dvdWriterNumberSelect.value) || 1;
-
-    dvdWriterNumber.innerHTML = '';
-
-    for (let index = 1; index <= rowCount; index += 1) {
-        const row = dvdWriterRowTemplate.cloneNode(true);
-        updateDVDWriterFieldIds(row, index);
-
-        if (index > 1) {
-            row.querySelectorAll('select').forEach((select) => {
-                select.selectedIndex = 0;
-            });
-        }
-
-        dvdWriterNumber.appendChild(row);
-    }
-}
-
 slotNumberSelect.addEventListener('change', renderRamRows);
 hardNumberSelect.addEventListener('change', renderHardRows);
-dvdWriterNumberSelect.addEventListener('change', renderDVDWriterRows);
 dvdWriterEnabled.addEventListener('change', toggleDVDWriterSection);
 toggleDVDWriterSection();
 renderRamRows();
 renderHardRows();
-renderDVDWriterRows();
